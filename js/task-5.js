21742fd8a3bd9b2b086f2801f02d4e81b7b67a65
vinvas94 <vintoniv1111@gmail.com>
2023-10-18 20:29:06 +0200

    add 5

--- js/task-5.js
+++ js/task-5.js
@@ -3,3 +3,31 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const refs = {
+  input: document.querySelector("input"),
+  buttonCreate: document.querySelector("button[data-create]"),
+  buttonDestroy: document.querySelector("button[data-destroy]"),
+  boxEl: document.querySelector("#boxes"),
+};
+
+function createBoxes(amount) {
+  for (let i = 0; i < amount; i++) {
+    const box = document.createElement("div");
+    box.style.width = `${30 + i * 10}px`;
+    box.style.height = `${30 + i * 10}px`;
+    box.style.backgroundColor = getRandomHexColor();
+    refs.boxEl.appendChild(box);
+  }
+}
+
+function destroyBoxes() {
+  boxesContainer.innerHTML = "";
+}
+
+refs.buttonCreate.addEventListener("click", () => {
+  const amount = parseInt(refs.input.value);
+  createBoxes(amount);
+});
+
+refs.buttonDestroy.addEventListener("click", destroyBoxes);
